27eb179249cac89c0eb3d0272d52addaca34502f
Michael Werfring <michaelmaximilianw@gmail.com>
2024-03-22 16:50:08 +0100

    Added grammar and simple calls in exe

--- asp_interpreter_exe/Program.cs
+++ asp_interpreter_exe/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Antlr4.Runtime;
+using asp_interpreter_lib.ANTLR;
+
+var program = @"node(a).
+                node(b).
+                node(c).
+                edge(a, b).
+                edge(b, c).";
+
+var inputStream = new AntlrInputStream(program);
+var lexer = new ASPLexer(inputStream);
+var commonTokenStream = new CommonTokenStream(lexer);
+var parser = new ASPParser(commonTokenStream);
+
+var context = parser.program();
+var visitor = new BasicAspVisitor();
+visitor.Visit(context);
