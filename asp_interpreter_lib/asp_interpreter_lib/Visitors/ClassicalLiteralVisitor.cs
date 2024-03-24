b303700dedd14defc01373a7d593eb8fa97c935d
Michael Werfring <michaelmaximilianw@gmail.com>
2024-03-24 09:40:02 +0100

    #6 - Fixed Duplicate Terms bug

--- asp_interpreter_lib/asp_interpreter_lib/Visitors/ClassicalLiteralVisitor.cs
+++ asp_interpreter_lib/asp_interpreter_lib/Visitors/ClassicalLiteralVisitor.cs
@@ -1,4 +1,5 @@
-﻿using asp_interpreter_lib.Types;
+﻿using System.Net;
+using asp_interpreter_lib.Types;
 using asp_interpreter_lib.Types.Terms;
 using asp_interpreter_lib.Visitors.TermVisitors;
 
@@ -9,26 +10,26 @@ public class ClassicalLiteralVisitor : ASPBaseVisitor<ClassicalLiteral>
     public override ClassicalLiteral VisitClassical_literal(ASPParser.Classical_literalContext context)
     {
         var negated = context.MINUS() !=  null;
-        var id = context.ID().GetText();
+        var id = context.ID().GetText() ?? 
+                 throw new ArgumentException("The given literal has no id!");
         
         List<Term> terms = [];
         var termVisitor = new TermVisitor();
         
-        var term = context.terms();
-                
-        terms.Add(term.Accept(termVisitor));
-
-        var childTerms = term.terms();
+        var childTerms = context.terms().children;
 
         if (childTerms == null)
         {
-            return new ClassicalLiteral(id, negated, terms); 
+            return new ClassicalLiteral(id, negated, terms);
         }
         
-        foreach (var t in term.terms().children)
+        foreach (var t in childTerms)
         {
-            Term te = t.Accept(termVisitor);
-            terms.Add(te);
+            Term term = t.Accept(termVisitor);
+            if (term != null)
+            {
+                terms.Add(term);
+            }
         }
         
         return new ClassicalLiteral(id, negated, terms);
