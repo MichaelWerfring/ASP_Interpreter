73e38d40abf6cf0a136a61dc130d3bc1b178ed2f
Michael Werfring <michaelmaximilianw@gmail.com>
2024-04-07 20:01:32 +0200

    #38 - implemented subcheck generation

---
+++ asp_interpreter_lib/Solving/NMRCheck/NmrChecker.cs
@@ -0,0 +1,42 @@
+﻿using asp_interpreter_lib.Types;
+
+namespace asp_interpreter_lib.Solving.NMRCheck;
+
+public class NmrChecker
+{
+    public static List<Statement> GetSubCheckRules(List<Statement> olonRules)
+    {
+        List<Statement> subCheckRules = [];
+        List<string> ruleNames = [];
+        
+        foreach (var olonRule in olonRules)
+        {
+            string ruleName = ASPExtensions.GenerateUniqeName(
+                olonRule.Head?.Literal?.Identifier ?? "empty_head",
+                ruleNames, "chk");
+            
+            
+            foreach (var literal in olonRule.Body.Literals)
+            {
+                Statement subcheck = new();
+                
+                subcheck.AddHead(new Head(new ClassicalLiteral(
+                        ruleName,
+                        literal.ClassicalLiteral.Negated,
+                        literal.ClassicalLiteral.Terms
+                        )));
+                
+                subcheck.AddBody(new Body([
+                    new NafLiteral(new ClassicalLiteral(
+                        literal.ClassicalLiteral.Identifier,
+                        false,
+                        literal.ClassicalLiteral.Terms), !literal.IsNafNegated)
+                ]));
+                
+                subCheckRules.Add(subcheck);
+            }
+        }
+        
+        return subCheckRules;
+    }
+}
