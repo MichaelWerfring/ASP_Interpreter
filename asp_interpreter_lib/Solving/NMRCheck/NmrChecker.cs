a5c99e4bbdf7d393b043f046e9baadc8cb8d995c
Michael Werfring <michaelmaximilianw@gmail.com>
2024-04-13 15:45:57 +0200

    #44 - Changed NMR Checker to work with new Literal

--- asp_interpreter_lib/Solving/NMRCheck/NmrChecker.cs
+++ asp_interpreter_lib/Solving/NMRCheck/NmrChecker.cs
@@ -1,41 +1,57 @@
 ﻿using asp_interpreter_lib.Types;
+using asp_interpreter_lib.Types.TypeVisitors;
 
 namespace asp_interpreter_lib.Solving.NMRCheck;
 
 public class NmrChecker
 {
+    private static GoalToLiteralConverter _goalToLiteralConverter = new();
+
     public static List<Statement> GetSubCheckRules(List<Statement> olonRules)
     {
         List<Statement> subCheckRules = [];
         List<string> ruleNames = [];
         
-        //foreach (var olonRule in olonRules)
-        //{
-        //    string ruleName = ASPExtensions.GenerateUniqeName(
-        //        olonRule.Head?.Literal?.Identifier ?? "empty_head",
-        //        ruleNames, "chk");
-        //    
-        //    
-        //    foreach (var literal in olonRule.Body.Literals)
-        //    {
-        //        Statement subcheck = new();
-        //        
-        //        subcheck.AddHead(new Head(new ClassicalLiteral(
-        //                ruleName,
-        //                literal.ClassicalLiteral.Negated,
-        //                literal.ClassicalLiteral.Terms
-        //                )));
-        //        
-        //        subcheck.AddBody(new Body([
-        //            new NafLiteral(new ClassicalLiteral(
-        //                literal.ClassicalLiteral.Identifier,
-        //                false,
-        //                literal.ClassicalLiteral.Terms), !literal.IsNafNegated)
-        //        ]));
-        //        
-        //        subCheckRules.Add(subcheck);
-        //    }
-        //}
+        foreach (var olonRule in olonRules)
+        {
+            if (!olonRule.HasHead)
+            {
+                //Skip for now add forall here later
+                continue;
+            }
+            
+            
+            string ruleName = ASPExtensions.GenerateUniqeName(
+                olonRule.Head.GetValueOrThrow().Identifier ?? "empty_head",
+                ruleNames, "chk");
+            
+            foreach (var goal in olonRule.Body)
+            {
+                Statement subcheck = new();
+                var literal = goal.Accept(_goalToLiteralConverter);
+
+                if (!literal.HasValue)
+                {
+                    //Skip for now then it is
+                    //something else than literal
+                    continue;
+                }
+                
+                subcheck.AddHead(new Literal(
+                    ruleName,
+                    false, 
+                    literal.GetValueOrThrow().HasStrongNegation,
+                    literal.GetValueOrThrow().Terms));
+                
+                subcheck.AddBody([new Literal(
+                    literal.GetValueOrThrow().Identifier,
+                    !literal.GetValueOrThrow().HasNafNegation,
+                    literal.GetValueOrThrow().HasStrongNegation,
+                    literal.GetValueOrThrow().Terms)]);
+                
+                subCheckRules.Add(subcheck);
+            }
+        }
         
         return subCheckRules;
     }
