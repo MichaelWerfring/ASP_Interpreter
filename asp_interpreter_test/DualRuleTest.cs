b67d5a35ef049c674d3aa46d92f91227320026b4
Michael Werfring <michaelmaximilianw@gmail.com>
2024-04-18 17:26:37 +0200

    #40 - Derived Tests for NMR Check from papers/presentations

--- asp_interpreter_test/DualRuleTest.cs
+++ asp_interpreter_test/DualRuleTest.cs
@@ -11,6 +11,9 @@ namespace asp_interpreter_test;
 
 public class DualRuleTest
 {
+    private readonly PrefixOptions _prefixes = new PrefixOptions(
+        "rwh", "fa", "eh", "chk");
+    
     [Test]
     public void TransformHeadHandlesCompoundTerms()
     {
@@ -21,7 +24,7 @@ public class DualRuleTest
         var errorLogger = new MockErrorLogger();
         var program = ASPExtensions.GetProgram(code, errorLogger);
         
-        var dualRuleConverter = new DualRuleConverter(program, new DualConverterOptions("rwh", "fa"));
+        var dualRuleConverter = new DualRuleConverter(program, _prefixes);
         var duals = dualRuleConverter.GetDualRules(program.Statements);
 
         Assert.That(duals[0].ToString(), Is.EqualTo("not a(X, b(rwh0_X, Y)) :- rwh0_X \\= X."));
@@ -38,7 +41,7 @@ public class DualRuleTest
         var errorLogger = new MockErrorLogger();
         var program = ASPExtensions.GetProgram(code, errorLogger);
         
-        var dualRuleConverter = new DualRuleConverter(program, new DualConverterOptions("rwh", "fa"));
+        var dualRuleConverter = new DualRuleConverter(program, _prefixes);
         var duals = dualRuleConverter.GetDualRules(program.Statements);
        
         Assert.That(duals.Count == 3 &&
@@ -57,7 +60,7 @@ public class DualRuleTest
                       """;
 
         var program = ASPExtensions.GetProgram(code, new MockErrorLogger());
-        var dualRuleConverter = new DualRuleConverter(program, new DualConverterOptions("rwh", "fa"));
+        var dualRuleConverter = new DualRuleConverter(program, _prefixes);
         var duals = dualRuleConverter.GetDualRules(program.Statements);
 
         //not p(X, Y) :- q(X).
@@ -81,7 +84,7 @@ public class DualRuleTest
         var errorLogger = new MockErrorLogger();
         var program = ASPExtensions.GetProgram(code, errorLogger);
         
-        var dualRuleConverter = new DualRuleConverter(program, new DualConverterOptions("rwh", "fa"));
+        var dualRuleConverter = new DualRuleConverter(program, _prefixes);
         var duals = dualRuleConverter.GetDualRules(program.Statements);
             
         Assert.That(duals.Count == 2 && errorLogger.Errors.Count == 0 &&
@@ -100,7 +103,7 @@ public class DualRuleTest
         var errorLogger = new MockErrorLogger();
         var program = ASPExtensions.GetProgram(code, errorLogger);
 
-        var dualRuleConverter = new DualRuleConverter(program, new DualConverterOptions("rwh", "fa"));
+        var dualRuleConverter = new DualRuleConverter(program, _prefixes);
         var duals = dualRuleConverter.GetDualRules(program.Statements);
         
         Assert.That(duals.Count == 2 && errorLogger.Errors.Count == 0 &&
@@ -121,7 +124,7 @@ public class DualRuleTest
         var errorLogger = new MockErrorLogger();
         var program = ASPExtensions.GetProgram(code, errorLogger);
 
-        var dualRuleConverter = new DualRuleConverter(program, new DualConverterOptions("rwh", "fa"));
+        var dualRuleConverter = new DualRuleConverter(program, _prefixes);
         var duals = dualRuleConverter.GetDualRules(program.Statements);
         
         Assert.That(errorLogger.Errors.Count == 0 && duals.Count == 5 &&
@@ -142,7 +145,7 @@ public class DualRuleTest
                       """;
 
         var program = ASPExtensions.GetProgram(code, new MockErrorLogger());
-        var dualRuleConverter = new DualRuleConverter(program, new DualConverterOptions("rwh", "fa"));
+        var dualRuleConverter = new DualRuleConverter(program, _prefixes);
         var duals = dualRuleConverter.GetDualRules(program.Statements);
         
         Assert.That(duals.Count == 0);
@@ -158,7 +161,7 @@ public class DualRuleTest
 
         var program = ASPExtensions.GetProgram(code, new MockErrorLogger());
         
-        var dualRuleConverter = new DualRuleConverter(program, new DualConverterOptions("rwh", "fa"));
+        var dualRuleConverter = new DualRuleConverter(program, _prefixes);
         var dual = dualRuleConverter.GetDualRules(program.Statements);   
         
         Assert.That(dual.Count == 1 && dual[0].ToString() == "not p(rwh0_3) :- rwh0_3 \\= 3.");
@@ -174,7 +177,7 @@ public class DualRuleTest
 
         var program = ASPExtensions.GetProgram(code, new MockErrorLogger());
         
-        var dualRuleConverter = new DualRuleConverter(program, new DualConverterOptions("rwh", "fa"));
+        var dualRuleConverter = new DualRuleConverter(program, _prefixes);
         var dual = dualRuleConverter.GetDualRules(program.Statements);
 
         Assert.That(dual.Count == 3 &&
@@ -193,7 +196,7 @@ public class DualRuleTest
 
         var program = ASPExtensions.GetProgram(code, new MockErrorLogger());
         
-        var dualRuleConverter = new DualRuleConverter(program, new DualConverterOptions("rwh", "fa"));
+        var dualRuleConverter = new DualRuleConverter(program, _prefixes);
         var dual = dualRuleConverter.GetDualRules(program.Statements);   
         
         Assert.That(dual.Count == 4 && 
@@ -213,7 +216,7 @@ public class DualRuleTest
 
         var program = ASPExtensions.GetProgram(code, new MockErrorLogger());
         
-        var dualRuleConverter = new DualRuleConverter(program, new DualConverterOptions("rwh", "fa"));
+        var dualRuleConverter = new DualRuleConverter(program, _prefixes);
         var dual = dualRuleConverter.GetDualRules(program.Statements);   
         
         Assert.That(dual.Count == 4 && 
@@ -233,7 +236,7 @@ public class DualRuleTest
 
         var program = ASPExtensions.GetProgram(code, new MockErrorLogger());
         
-        var dualRuleConverter = new DualRuleConverter(program, new DualConverterOptions("rwh", "fa"));
+        var dualRuleConverter = new DualRuleConverter(program, _prefixes);
         var dual = dualRuleConverter.GetDualRules(program.Statements);
 
         string d1 = dual[0].ToString();
--- asp_interpreter_test/NmrCheckerTest.cs
+++ asp_interpreter_test/NmrCheckerTest.cs
@@ -6,106 +6,12 @@ namespace asp_interpreter_test;
 
 public class NmrCheckerTest
 {
-    [Test]
-    public void NmrCheckerComputesSubcheck()
-    {
-        string code = """
-                      p :- r, not p.
-                      p :- not q, not p.
-                      p :- q, r, not p.
-                      p?
-                      """;
-        var errorLogger = new MockErrorLogger();
-        var program = ASPExtensions.GetProgram(code, errorLogger);
-        
-        var subCheckRules = NmrChecker.GetSubCheckRules(program.Statements);
-        
-        Assert.That(subCheckRules.Count == 8 &&
-                    subCheckRules[0].ToString() == "nmr_check :- chk0_p, chk1_p, chk2_p." &&
-                    subCheckRules[1].ToString() == "chk0_p :- not r." &&
-                    subCheckRules[2].ToString() == "chk0_p :- p." &&
-                    subCheckRules[3].ToString() == "chk1_p :- q." &&
-                    subCheckRules[4].ToString() == "chk1_p :- p." &&
-                    subCheckRules[5].ToString() == "chk2_p :- not q." &&
-                    subCheckRules[6].ToString() == "chk2_p :- not r." &&
-                    subCheckRules[7].ToString() == "chk2_p :- p.");
-    }
+    private readonly PrefixOptions _prefixes = new PrefixOptions(
+        "rwh", "fa", "eh", "chk_");
+    
     
     [Test]
-    public void NmrCheckerComputesSubcheckWithEmptyHead()
-    {
-        string code = """
-                      :- r, not p.
-                      :- not q, not p.
-                      :- q, r, not p.
-                      p?
-                      """;
-        var errorLogger = new MockErrorLogger();
-        var program = ASPExtensions.GetProgram(code, errorLogger);
-        
-        var subCheckRules = NmrChecker.GetSubCheckRules(program.Statements);
-        
-        Assert.That(subCheckRules.Count == 8 &&
-                    subCheckRules[0].ToString() == "nmr_check :- chk0_, chk1_, chk2_." &&
-                    subCheckRules[1].ToString() == "chk0_ :- not r." &&
-                    subCheckRules[2].ToString() == "chk0_ :- p." &&
-                    subCheckRules[3].ToString() == "chk1_ :- q." &&
-                    subCheckRules[4].ToString() == "chk1_ :- p." &&
-                    subCheckRules[5].ToString() == "chk2_ :- not q." &&
-                    subCheckRules[6].ToString() == "chk2_ :- not r." &&
-                    subCheckRules[7].ToString() == "chk2_ :- p.");
-    }
-    
-    [Test]
-    public void NmrCheckerComputesSubcheckWithVariables()
-    {
-        string code = """
-                      p(X) :- r(X), not p(X).
-                      q(X) :- not q(X), not p(X).
-                      r(X) :- q(X), r(X), not p(X).
-                      p(a)?
-                      """;
-        var errorLogger = new MockErrorLogger();
-        var program = ASPExtensions.GetProgram(code, errorLogger);
-        
-        var subCheckRules = NmrChecker.GetSubCheckRules(program.Statements);
-        
-        Assert.That(subCheckRules.Count == 8 &&
-                    subCheckRules[0].ToString() == "nmr_check :- chk0_p(X), chk1_p(X), chk2_p(X)." &&
-                    subCheckRules[1].ToString() == "chk0_p(X) :- not r(X)." &&
-                    subCheckRules[2].ToString() == "chk0_p(X) :- p(X)." &&
-                    subCheckRules[3].ToString() == "chk1_p(X) :- q(X)." &&
-                    subCheckRules[4].ToString() == "chk1_p(X) :- p(X)." &&
-                    subCheckRules[5].ToString() == "chk2_p(X) :- not q(X)." &&
-                    subCheckRules[6].ToString() == "chk2_p(X) :- not r(X)." &&
-                    subCheckRules[7].ToString() == "chk2_p(X) :- p(X).");
-    }
-
-    [Test]
-    public void NmrCheckComputesSubcheckWithDifferentHeads()
-    {
-        string code = """
-                      p(X) :- r(X), not p(X).
-                      p :- not q, not p.
-                      :- q.
-                      p(a)?
-                      """;
-        var errorLogger = new MockErrorLogger();
-        var program = ASPExtensions.GetProgram(code, errorLogger);
-        
-        var subCheckRules = NmrChecker.GetSubCheckRules(program.Statements);
-        
-        Assert.That(subCheckRules.Count == 6 &&
-                    subCheckRules[0].ToString() == "nmr_check :- chk0_p(X), chk1_p, chk0_." &&
-                    subCheckRules[1].ToString() == "chk0_p(X) :- not r(X)." &&
-                    subCheckRules[2].ToString() == "chk0_p(X) :- p(X)." &&
-                    subCheckRules[3].ToString() == "chk1_p :- q." &&
-                    subCheckRules[4].ToString() == "chk1_p :- p." &&
-                    subCheckRules[5].ToString() == "chk0_ :- not q.");
-    }
-
-    [Test]
-    public void NmrCheckWorksWithDualRules()
+    public void NmrCheckHandlesBasicProgram()
     {
         string code = """
                       p(X) :- q(X), not p(X).
@@ -113,26 +19,93 @@ public class NmrCheckerTest
                       """;
         var errorLogger = new MockErrorLogger();
         var program = ASPExtensions.GetProgram(code, errorLogger);
-        
-        var subCheckRules = NmrChecker.GetSubCheckRules(program.Statements);
+        var checker = new NmrChecker(_prefixes);    
+        var subCheckRules = checker.GetSubCheckRules(program.Statements);
         
         Assert.That(subCheckRules.Count == 3 &&
-                    subCheckRules[0].ToString() == "nmr_check :- forall(X, chk0_p(X))." &&
-                    subCheckRules[1].ToString() == "chk0_p(X) :- not q(X)." &&
-                    subCheckRules[2].ToString() == "chk0_p(X) :- q(X), p(X).");
+                    subCheckRules[0].ToString() == "nmr_check :- forall(X, chk_p(X))." &&
+                    subCheckRules[1].ToString() == "chk_p(X) :- not q(X)." &&
+                    subCheckRules[2].ToString() == "chk_p(X) :- q(X), p(X).");
     }
-
+    
     [Test]
-    public void NmrCheckHandlesComplexRulesAndForall()
+    public void NmrCheckHandlesEmptyRuleHeads()
     {
         string code = """
-                      p(X) :- q(X, Y), not p(Y).
-
+                      :- not r(X).
                       p(a)?
                       """;
         var errorLogger = new MockErrorLogger();
         var program = ASPExtensions.GetProgram(code, errorLogger);
+        var checker = new NmrChecker(_prefixes);    
+        var subCheckRules = checker.GetSubCheckRules(program.Statements);
         
-        var subCheckRules = NmrChecker.GetSubCheckRules(program.Statements);
+        Assert.That(subCheckRules.Count == 3 &&
+                    subCheckRules[0].ToString() == "nmr_check :- chk_eh0." &&
+                    subCheckRules[1].ToString() == "chk_eh0 :- forall(X, fa0_eh0(X))." &&
+                    subCheckRules[2].ToString() == "fa0_eh0(X) :- r(X).");
+    }
+    
+    [Test]
+    public void NmrCheckHandlesCompoundProgram()
+    {
+        string code = """
+                      p(X) :- q(X), not p(X).
+                      :- not r(X).
+                      p(a)?
+                      """;
+        var errorLogger = new MockErrorLogger();
+        var program = ASPExtensions.GetProgram(code, errorLogger);
+        var checker = new NmrChecker(_prefixes);    
+        var subCheckRules = checker.GetSubCheckRules(program.Statements);
+        
+        Assert.That(subCheckRules.Count == 5 &&
+                    subCheckRules[0].ToString() == "nmr_check :- forall(X, chk_p(X)), chk_eh0." &&
+                    subCheckRules[1].ToString() == "chk_p(X) :- not q(X)." &&
+                    subCheckRules[2].ToString() == "chk_p(X) :- q(X), p(X)." &&
+                    subCheckRules[3].ToString() == "chk_eh0 :- forall(X, fa0_eh0(X))." &&
+                    subCheckRules[4].ToString() == "fa0_eh0(X) :- r(X).");
+    }
+    
+    [Test]
+    public void NmrCheckHandlesCompoundProgramWithAtom()
+    {
+        string code = """
+                      :- not s(1, X).
+                      p(X):- q(X), not p(X).
+                      p(a)?
+                      """;
+        var errorLogger = new MockErrorLogger();
+        var program = ASPExtensions.GetProgram(code, errorLogger);
+        var checker = new NmrChecker(_prefixes);    
+        var subCheckRules = checker.GetSubCheckRules(program.Statements);
+        
+        Assert.That(subCheckRules.Count == 5 &&
+                    subCheckRules[0].ToString() == "nmr_check :- chk_eh0, forall(X, chk_p(X))." &&
+                    subCheckRules[1].ToString() == "chk_eh0 :- forall(X, fa0_eh0(X))." &&
+                    subCheckRules[2].ToString() == "fa0_eh0(X) :- s(1, X)." &&
+                    subCheckRules[3].ToString() == "chk_p(X) :- not q(X)." &&
+                    subCheckRules[4].ToString() == "chk_p(X) :- q(X), p(X).");
+    }
+
+    [Test]
+    public void NmrCheckAddsHeadOfRuleToBodyIfNecessary()
+    {
+        string code = """
+                      p(X) :- q(X, Y), not p(Y).
+                      p(a)?
+                      """;
+        var errorLogger = new MockErrorLogger();
+        var program = ASPExtensions.GetProgram(code, errorLogger);
+        var checker = new NmrChecker(_prefixes);    
+        var subCheckRules = checker.GetSubCheckRules(program.Statements);
+        
+        Assert.That(subCheckRules.Count == 5 &&
+                    subCheckRules[0].ToString() == "nmr_check :- forall(X, chk_p(X))." &&
+                    subCheckRules[1].ToString() == "chk_p(X) :- forall(Y, fa0_p(X, Y))." &&
+                    subCheckRules[2].ToString() == "fa0_p(X, Y) :- not q(X, Y)." &&
+                    subCheckRules[3].ToString() == "fa0_p(X, Y) :- q(X, Y), p(Y)." &&
+                    subCheckRules[4].ToString() == "fa0_p(X, Y) :- q(X, Y), not p(Y), p(X).");
+
     }
 }
