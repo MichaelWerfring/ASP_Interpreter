f9b4807b82953459ab08914aad769aa86f4f77b8
Michael Werfring <michaelmaximilianw@gmail.com>
2024-05-27 16:58:17 +0200

    #71 - Documentation of Variable finder

--- asp_interpreter_lib/Types/TypeVisitors/VariableFinder.cs
+++ asp_interpreter_lib/Types/TypeVisitors/VariableFinder.cs
@@ -12,13 +12,21 @@ using Asp_interpreter_lib.Types.BinaryOperations;
 using Asp_interpreter_lib.Types.Terms;
 using Asp_interpreter_lib.Util.ErrorHandling;
 
+/// <summary>
+/// Iterates through a program and returns a list of all variables in the program.
+/// </summary>
 public class VariableFinder : TypeBaseVisitor<List<VariableTerm>>
 {
-    /// <inheritdoc/>
+    /// <summary>
+    /// Retrieves all variables from a given program.
+    /// </summary>
+    /// <param name="program">The program to retrieve variables from.</param>
+    /// <returns>None if the process failed else a list of variables found.</returns>
+    /// <exception cref="ArgumentNullException">If the program is null.</exception>
     public override IOption<List<VariableTerm>> Visit(AspProgram program)
     {
         ArgumentNullException.ThrowIfNull(program);
-        List<VariableTerm> variableTerms =[];
+        List<VariableTerm> variableTerms = new List<VariableTerm>();
         foreach (var statement in program.Statements)
         {
             statement.Accept(this).IfHasValue(variableTerms.AddRange);
@@ -29,11 +37,16 @@ public class VariableFinder : TypeBaseVisitor<List<VariableTerm>>
         return new Some<List<VariableTerm>>(variableTerms);
     }
 
-    /// <inheritdoc/>
+    /// <summary>
+    /// Retrieves all variables from a given statement.
+    /// </summary>
+    /// <param name="statement">The statement to retrieve the variables from.</param>
+    /// <returns>None if the process failed else a list of variables found.</returns>
+    /// <exception cref="ArgumentNullException">If the statement is null.</exception>
     public override IOption<List<VariableTerm>> Visit(Statement statement)
     {
         ArgumentNullException.ThrowIfNull(statement);
-        List<VariableTerm> variables =[];
+        List<VariableTerm> variables = new List<VariableTerm>();
 
         if (statement.HasHead)
         {
@@ -52,11 +65,16 @@ public class VariableFinder : TypeBaseVisitor<List<VariableTerm>>
         return new Some<List<VariableTerm>>(variables);
     }
 
-    /// <inheritdoc/>
+    /// <summary>
+    ///  Retrieves all variables from a given literal.
+    /// </summary>
+    /// <param name="literal">The literal to retrieve the variables from.</param>
+    /// <returns>None if the process failed else a list of variables found.</returns>
+    /// <exception cref="ArgumentNullException">If the literal is null.</exception>
     public override IOption<List<VariableTerm>> Visit(Literal literal)
     {
         ArgumentNullException.ThrowIfNull(literal);
-        List<VariableTerm> variables =[];
+        List<VariableTerm> variables = new List<VariableTerm>();
         foreach (var term in literal.Terms)
         {
             term.Accept(this).IfHasValue(variables.AddRange);
@@ -65,35 +83,66 @@ public class VariableFinder : TypeBaseVisitor<List<VariableTerm>>
         return new Some<List<VariableTerm>>(variables);
     }
 
-    /// <inheritdoc/>
+    /// <summary>
+    /// Visits the given operation.
+    /// </summary>
+    /// <param name="plus">The operation to visit.</param>
+    /// <returns>An empty list.</returns>
     public override IOption<List<VariableTerm>> Visit(Plus plus)
     {
         return new Some<List<VariableTerm>>([]);
     }
 
-    /// <inheritdoc/>
+    /// <summary>
+    /// Visits the given operation.
+    /// </summary>
+    /// <param name="minus">The operation to visit.</param>
+    /// <returns>An empty list.</returns>
     public override IOption<List<VariableTerm>> Visit(Minus minus)
     {
         return new Some<List<VariableTerm>>([]);
     }
 
-    /// <inheritdoc/>
+    /// <summary>
+    /// Visits the given operation.
+    /// </summary>
+    /// <param name="multiply">The operation to visit.</param>
+    /// <returns>An empty list.</returns>
     public override IOption<List<VariableTerm>> Visit(Multiply multiply)
     {
         return new Some<List<VariableTerm>>([]);
     }
 
-    /// <inheritdoc/>
+    /// <summary>
+    /// Visits the given operation.
+    /// </summary>
+    /// <param name="power">The operation to visit.</param>
+    /// <returns>An empty list.</returns>
+    public override IOption<List<VariableTerm>> Visit(Power power)
+    {
+        return new Some<List<VariableTerm>>([]);
+    }
+
+    /// <summary>
+    /// Visits the given operation.
+    /// </summary>
+    /// <param name="divide">The operation to visit.</param>
+    /// <returns>An empty list.</returns>
     public override IOption<List<VariableTerm>> Visit(Divide divide)
     {
         return new Some<List<VariableTerm>>([]);
     }
 
-    /// <inheritdoc/>
+    /// <summary>
+    /// Retrieves all variables from a given binary operation.
+    /// </summary>
+    /// <param name="binOp">The operation to retrieve the variables from.</param>
+    /// <returns>None if the process failed else a list of variables found.</returns>
+    /// <exception cref="ArgumentNullException">If the operation is null.</exception>
     public override IOption<List<VariableTerm>> Visit(BinaryOperation binOp)
     {
         ArgumentNullException.ThrowIfNull(binOp);
-        List<VariableTerm> variables =[];
+        List<VariableTerm> variables = new List<VariableTerm>();
 
         binOp.Left.Accept(this).IfHasValue(variables.AddRange);
 
@@ -102,66 +151,107 @@ public class VariableFinder : TypeBaseVisitor<List<VariableTerm>>
         return new Some<List<VariableTerm>>(variables);
     }
 
-    /// <inheritdoc/>
+    /// <summary>
+    /// Returns an empty list because the given type cannot contain any variables.
+    /// </summary>
+    /// <param name="_">The given operation.</param>
+    /// <returns>An empty list.</returns>
     public override IOption<List<VariableTerm>> Visit(Disunification _)
     {
         return new Some<List<VariableTerm>>([]);
     }
 
-    /// <inheritdoc/>
+    /// <summary>
+    /// Returns an empty list because the given type cannot contain any variables.
+    /// </summary>
+    /// <param name="_">The given operation.</param>
+    /// <returns>An empty list.</returns>
     public override IOption<List<VariableTerm>> Visit(Equality _)
     {
         return new Some<List<VariableTerm>>([]);
     }
 
-    /// <inheritdoc/>
+    /// <summary>
+    /// Returns an empty list because the given type cannot contain any variables.
+    /// </summary>
+    /// <param name="_">The given operation.</param>
+    /// <returns>An empty list.</returns>
     public override IOption<List<VariableTerm>> Visit(GreaterOrEqualThan _)
     {
         return new Some<List<VariableTerm>>([]);
     }
 
-    /// <inheritdoc/>
+    /// <summary>
+    /// Returns an empty list because the given type cannot contain any variables.
+    /// </summary>
+    /// <param name="_">The given operation.</param>
+    /// <returns>An empty list.</returns>
     public override IOption<List<VariableTerm>> Visit(GreaterThan _)
     {
         return new Some<List<VariableTerm>>([]);
     }
 
-    /// <inheritdoc/>
+    /// <summary>
+    /// Returns an empty list because the given type cannot contain any variables.
+    /// </summary>
+    /// <param name="_">The given operation.</param>
+    /// <returns>An empty list.</returns>
     public override IOption<List<VariableTerm>> Visit(LessOrEqualThan _)
     {
         return new Some<List<VariableTerm>>([]);
     }
 
-    /// <inheritdoc/>
+    /// <summary>
+    /// Returns an empty list because the given type cannot contain any variables.
+    /// </summary>
+    /// <param name="_">The given operation.</param>
+    /// <returns>An empty list.</returns>
     public override IOption<List<VariableTerm>> Visit(LessThan _)
     {
         return new Some<List<VariableTerm>>([]);
     }
 
-    /// <inheritdoc/>
+    /// <summary>
+    /// Returns an empty list because the given type cannot contain any variables.
+    /// </summary>
+    /// <param name="_">The given operation.</param>
+    /// <returns>An empty list.</returns>
     public override IOption<List<VariableTerm>> Visit(Is _)
     {
         return new Some<List<VariableTerm>>([]);
     }
 
-    /// <inheritdoc/>
-    public override IOption<List<VariableTerm>> Visit(AnonymousVariableTerm variable)
+    /// <summary>
+    /// Returns an empty list because the given type cannot contain any variables.
+    /// </summary>
+    /// <param name="_">The given operation.</param>
+    /// <returns>An empty list.</returns>
+    public override IOption<List<VariableTerm>> Visit(AnonymousVariableTerm _)
     {
         return new Some<List<VariableTerm>>([]);
     }
 
-    /// <inheritdoc/>
+    /// <summary>
+    /// Returns a list containing only the given variable.
+    /// </summary>
+    /// <param name="variable">The variable found.</param>
+    /// <returns>A list only containing the given variable.</returns>
     public override IOption<List<VariableTerm>> Visit(VariableTerm variable)
     {
         return new Some<List<VariableTerm>>([variable]);
     }
 
-    /// <inheritdoc/>
+    /// <summary>
+    /// Retrieves all variables from a given arithmetic operation.
+    /// </summary>
+    /// <param name="arithmeticOperation">The operation to retrieve the variables from.</param>
+    /// <returns>None if the process failed else a list of variables found.</returns>
+    /// <exception cref="ArgumentNullException">If the operation is null.</exception>
     public override IOption<List<VariableTerm>> Visit(ArithmeticOperationTerm arithmeticOperation)
     {
         ArgumentNullException.ThrowIfNull(arithmeticOperation);
 
-        List<VariableTerm> variables =[];
+        List<VariableTerm> variables = new List<VariableTerm>();
         variables.AddRange(arithmeticOperation.Left.Accept(this).
             GetValueOrThrow("Cannot get variables from arithmetic operation!"));
         variables.AddRange(arithmeticOperation.Right.Accept(this).
@@ -170,11 +260,16 @@ public class VariableFinder : TypeBaseVisitor<List<VariableTerm>>
         return new Some<List<VariableTerm>>(variables);
     }
 
-    /// <inheritdoc/>
+    /// <summary>
+    /// Retrieves all variables from a given basic term.
+    /// </summary>
+    /// <param name="basicTerm">The term to retrieve the variables form.</param>
+    /// <returns>A list containing all the variables from the given term.</returns>
+    /// <exception cref="ArgumentNullException">If the term is null.</exception>
     public override IOption<List<VariableTerm>> Visit(BasicTerm basicTerm)
     {
         ArgumentNullException.ThrowIfNull(basicTerm);
-        List<VariableTerm> variables =[];
+        List<VariableTerm> variables = new List<VariableTerm>();
 
         foreach (var term in basicTerm.Terms)
         {
@@ -184,24 +279,37 @@ public class VariableFinder : TypeBaseVisitor<List<VariableTerm>>
         return new Some<List<VariableTerm>>(variables);
     }
 
-    /// <inheritdoc/>
+    /// <summary>
+    /// Returns an empty list because the given type cannot contain any variables.
+    /// </summary>
+    /// <param name="_">The given term.</param>
+    /// <returns>An empty list.</returns>
     public override IOption<List<VariableTerm>> Visit(StringTerm _)
     {
         return new Some<List<VariableTerm>>([]);
     }
 
-    /// <inheritdoc/>
+    /// <summary>
+    /// Returns an empty list because the given type cannot contain any variables.
+    /// </summary>
+    /// <param name="_">The given term.</param>
+    /// <returns>An empty list.</returns>
     public override IOption<List<VariableTerm>> Visit(NumberTerm _)
     {
         return new Some<List<VariableTerm>>([]);
     }
 
-    /// <inheritdoc/>
-    public override IOption<List<VariableTerm>> Visit(NegatedTerm innerTerm)
+    /// <summary>
+    /// Retrieves all variables from a given negated term.
+    /// </summary>
+    /// <param name="term">The term to retrieve the variables from.</param>
+    /// <returns>All the variables contained in the term.</returns>
+    /// <exception cref="ArgumentNullException">Is thrown if the term is null.</exception>
+    public override IOption<List<VariableTerm>> Visit(NegatedTerm term)
     {
-        ArgumentNullException.ThrowIfNull(innerTerm);
-        List<VariableTerm> variables =[];
-        innerTerm.Term.Accept(this).IfHasValue(v =>
+        ArgumentNullException.ThrowIfNull(term);
+        List<VariableTerm> variables = new List<VariableTerm>();
+        term.Term.Accept(this).IfHasValue(v =>
         {
             variables = v;
         });
@@ -209,12 +317,17 @@ public class VariableFinder : TypeBaseVisitor<List<VariableTerm>>
         return new Some<List<VariableTerm>>(variables);
     }
 
-    /// <inheritdoc/>
-    public override IOption<List<VariableTerm>> Visit(ParenthesizedTerm innerTerm)
+    /// <summary>
+    /// Retrieves all variables from a given parenthesized term.
+    /// </summary>
+    /// <param name="term">The term to retrieve the variables from.</param>
+    /// <returns>All the variables contained in the term.</returns>
+    /// <exception cref="ArgumentNullException">Is thrown if the term is null.</exception>
+    public override IOption<List<VariableTerm>> Visit(ParenthesizedTerm term)
     {
-        ArgumentNullException.ThrowIfNull(innerTerm);
-        List<VariableTerm> variables =[];
-        innerTerm.Term.Accept(this).IfHasValue(v =>
+        ArgumentNullException.ThrowIfNull(term);
+        List<VariableTerm> variables = new List<VariableTerm>();
+        term.Term.Accept(this).IfHasValue(v =>
         {
             variables = v;
         });
@@ -222,11 +335,16 @@ public class VariableFinder : TypeBaseVisitor<List<VariableTerm>>
         return new Some<List<VariableTerm>>(variables);
     }
 
-    /// <inheritdoc/>
+    /// <summary>
+    /// Retrieves all variables from a given list term.
+    /// </summary>
+    /// <param name="list">The list to retrieve the variables from.</param>
+    /// <returns>A list of variables found.</returns>
+    /// <exception cref="ArgumentNullException">If the list is null.</exception>
     public override IOption<List<VariableTerm>> Visit(RecursiveList list)
     {
         ArgumentNullException.ThrowIfNull(list);
-        List<VariableTerm> terms =[];
+        List<VariableTerm> terms = new List<VariableTerm>();
 
         list.Head.Accept(this).IfHasValue(terms.AddRange);
         list.Tail.Accept(this).IfHasValue(terms.AddRange);
@@ -234,11 +352,16 @@ public class VariableFinder : TypeBaseVisitor<List<VariableTerm>>
         return new Some<List<VariableTerm>>(terms);
     }
 
-    /// <inheritdoc/>
+    /// <summary>
+    /// Retrieves all variables from a given list term.
+    /// </summary>
+    /// <param name="list">The list to retrieve the variables from.</param>
+    /// <returns>A list of variables found.</returns>
+    /// <exception cref="ArgumentNullException">If the list is null.</exception>
     public override IOption<List<VariableTerm>> Visit(ConventionalList list)
     {
         ArgumentNullException.ThrowIfNull(list);
-        List<VariableTerm> variables =[];
+        List<VariableTerm> variables = new List<VariableTerm>();
         foreach (var term in list.Terms)
         {
             term.Accept(this).IfHasValue(variables.AddRange);
@@ -247,7 +370,11 @@ public class VariableFinder : TypeBaseVisitor<List<VariableTerm>>
         return new Some<List<VariableTerm>>(variables);
     }
 
-    /// <inheritdoc/>
+    /// <summary>
+    /// Returns an empty list because the given type cannot contain any variables.
+    /// </summary>
+    /// <param name="_">The given term.</param>
+    /// <returns>An empty list.</returns>
     public override IOption<List<VariableTerm>> Visit(Forall _)
     {
         return new Some<List<VariableTerm>>([]);
